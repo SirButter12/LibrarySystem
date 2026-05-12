7c8e912f977284e9beb6c4e0929fd92dd2512801
SirButter12 <cianobed@gmail.com>
2026-05-11 20:54:12 -0400

    Item has a new enum called type that will be used in the future

--- src/main/java/org/example/users/User.java
+++ src/main/java/org/example/users/User.java
@@ -1,11 +1,20 @@
 package org.example.users;
+import org.example.LibrarySystem;
 import org.example.items.Item;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 
 public abstract class User {
     private String name;
     private String id;
     private static int nextId = 1;
+    private Set<Item.Type> borrowable = new HashSet<>();
+    private List<Item> BorowedItems = new ArrayList<>();
+    private static List<Item> itemsInLibrary = LibrarySystem.getItems();
 
     public User(String name) {
         this.name = name;
