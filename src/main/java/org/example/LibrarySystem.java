1fa487d46193bf69655ae3895dd86da43ac78560
SirButter12 <cianobed@gmail.com>
2026-05-11 22:39:43 -0400

    Library system has now different lists for the different kinds of items sorted by author and title, this uses more space but it will make it faster to search for items

--- src/main/java/org/example/LibrarySystem.java
+++ src/main/java/org/example/LibrarySystem.java
@@ -3,11 +3,52 @@ package org.example;
 import lombok.Getter;
 import org.example.items.Item;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class LibrarySystem {
     @Getter
     private static List<Item> items = new ArrayList<>();
+
+    private static List<Item> booksByTitle;
+    private static List<Item> booksByAuthor;
+
+    private static List<Item> DVDsByTitle;
+    private static List<Item> DVDsByDirector;
+
+    private static List<Item> magazinesByTitle;
+    private static List<Item> magazinesByPublisher;
+
+    private static Comparator<Item> titleComparator = new Item.titleComparator();
+    private static Comparator<Item> responsableComparator = new Item.ResponsableComparator();
+
+    public static void updateBooks() {
+        booksByTitle = items.stream().filter(item -> item.getType().equals(Item.Type.BOOK)).toList();
+        booksByAuthor = items.stream().filter(item -> item.getType().equals(Item.Type.BOOK)).toList();
+
+        booksByTitle.sort(titleComparator);
+        booksByAuthor.sort(responsableComparator);
+    }
+
+    public static void updateDVDs() {
+        DVDsByDirector = items.stream().filter(item -> item.getType().equals(Item.Type.DVD)).toList();
+        DVDsByTitle = items.stream().filter(item -> item.getType().equals(Item.Type.DVD)).toList();
+
+        DVDsByTitle.sort(titleComparator);
+        DVDsByDirector.sort(responsableComparator);
+    }
+
+    public static void updateMagazines() {
+        magazinesByTitle = items.stream().filter(item -> item.getType().equals(Item.Type.MAGAZINE)).toList();
+        magazinesByPublisher = items.stream().filter(item -> item.getType().equals(Item.Type.MAGAZINE)).toList();
+
+        booksByTitle.sort(titleComparator);
+        magazinesByPublisher.sort(responsableComparator);
+    }
+
+    private void books() {
+    }
+
 
     public static boolean addItem(Item item) {
         if (items.contains(item)) {
@@ -15,6 +56,13 @@ public class LibrarySystem {
         }
 
         items.add(item);
+
+        switch (item.getType()) {
+            case BOOK -> updateBooks();
+            case DVD -> updateDVDs();
+            case MAGAZINE -> updateMagazines();
+        }
+
         return true;
     }
 
